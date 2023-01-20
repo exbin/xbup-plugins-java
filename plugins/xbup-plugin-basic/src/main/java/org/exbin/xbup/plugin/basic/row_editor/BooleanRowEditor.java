be5b1f2324a77e521f3e4959d46093b4f72cea8c
hajdam <hajdam@users.sf.net>
2023-01-20 23:21:19 +0100

    Headers cleanup

--- plugins/xbup-plugin-basic/src/main/java/org/exbin/xbup/plugin/basic/row_editor/BooleanRowEditor.java
+++ plugins/xbup-plugin-basic/src/main/java/org/exbin/xbup/plugin/basic/row_editor/BooleanRowEditor.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,15 +30,14 @@ import org.exbin.xbup.plugin.XBRowEditor;
 /**
  * XBUP Editor plugin - provides panels for basic XBUP data types.
  *
- * @version 0.2.1 2020/07/23
- * @author ExBin Project (http://exbin.org)
+ * @author ExBin Project (https://exbin.org)
  */
 public class BooleanRowEditor extends XBAbstractRowEditor implements XBRowEditor, XBPSequenceSerializable {
 
     private UBBool value = new UBBool();
     private static final String TRUE_TEXT = "true";
     private static final String FALSE_TEXT = "false";
-    
+
     private JCheckBox editor = null;
 
     @Override
--- plugins/xbup-plugin-demo/src/main/java/org/exbin/xbup/plugin/demo/panel_viewer/XmlDocumentPanelViewer.java
+++ plugins/xbup-plugin-demo/src/main/java/org/exbin/xbup/plugin/demo/panel_viewer/XmlDocumentPanelViewer.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -39,8 +39,7 @@ import org.exbin.xbup.plugin.XBPanelViewer;
 /**
  * XBUP viewer plugin - provides panels for basic XBUP data types.
  *
- * @version 0.2.1 2020/09/26
- * @author ExBin Project (http://exbin.org)
+ * @author ExBin Project (https://exbin.org)
  */
 @ParametersAreNonnullByDefault
 public class XmlDocumentPanelViewer extends XBAbstractPanelViewer implements XBPanelViewer, XBPSequenceSerializable {
@@ -96,7 +95,7 @@ public class XmlDocumentPanelViewer extends XBAbstractPanelViewer implements XBP
             serial.begin();
             serial.pullType();
             int attrCount = serial.pullIntAttribute();
-            
+
             for (int i = 0; i < attrCount; i++) {
                 serial.begin();
                 serial.pullType();
